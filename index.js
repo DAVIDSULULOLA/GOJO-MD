a0c69b3cb0c084afbc24f3c19a78871f5f709de9
davideals <148756522+DAVIDSULULOLA@users.noreply.github.com>
2024-04-14 14:34:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import cfonts from 'cfonts';
 
 const { say } = cfonts;
 
-say("XLICON - V2", {
+say("GOJO-MD", {
   'font': "block",
   'align': "center",
   'colors': ['#ff9900'],
@@ -23,7 +23,7 @@ say("XLICON - V2", {
   'maxLength': '15'
 });
 
-say("Xlicon-BOT-V2 By Salman._.", {
+say("GOJO-MD By DAVID SULULOLA._.", {
   'font': "chrome",
   'align': "center",
   'colors': ["red", "magenta"],
