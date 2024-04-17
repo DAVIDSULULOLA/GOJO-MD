b52ab74e4d4bf81a3386fabce1cc64719f56b3e8
davideals <148756522+DAVIDSULULOLA@users.noreply.github.com>
2024-04-17 14:17:58 +0100

    Update Main-DEVS.js

--- plugins/Main-DEVS.js
+++ plugins/Main-DEVS.js
@@ -2,12 +2,12 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "𝐇𝐞𝐲" }, "message":{ "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   let pp = './Assets/XLICON-V2.jpg'
 
-const cat = `*_XLICON-V2_*
+const cat = `*_GOJO MD_*
 
 
-*_—🔰 𝐗𝐥𝐢𝐜𝐨𝐧 𝐯𝟐  𝐃𝐄𝐕 𝟏 wa.me/+2348026018074 %dont_contact_me%_*
+*_—🔰 GOJO MD  𝐃𝐄𝐕 𝟏 wa.me/+2348026018074 %dont_contact_me%_*
 
-*_—🔰 𝐗𝐥𝐢𝐜𝐨𝐧 𝐯𝟐  𝐃𝐄𝐕 2 wa.me/+2348026018074_*
+*_—🔰 GOJO MD  𝐃𝐄𝐕 2 wa.me/+2348026018074_*
 *---------------------*
 
 
